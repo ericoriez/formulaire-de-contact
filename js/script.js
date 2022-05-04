0b76b515abadb4cc05a1eb3192cb543afab7d194
ericoriez <95495656+ericoriez@users.noreply.github.com>
2022-05-04 17:02:27 +0200

    fetch

--- js/script.js
+++ js/script.js
@@ -6,41 +6,123 @@ const message = document.getElementById('message');
 const msgError = document.querySelectorAll('.error');
 const btn = document.getElementById('btn');
 const succes = document.querySelector('.succes')
-console.log(btn);
 
-// je detecte la validation du formulaire
 form.addEventListener('submit', function(e){
     e.preventDefault();
+
+    let data = "nom="+ nom.value + "&email="+ email.value + "&sujet="+ sujet.value + "&message="+ message.value;
+    console.log(data);
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", "./php/contact.php", true);
+    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    xhr.send(data);
+
+    // let value = {nomValue, emailValue, sujetValue, messageValue};
+    // console.log(value);
     
-    // je recupere les valeurs de chacun des inputs
-    const nomValue = nom.value.trim();
-    console.log(nomValue);
-    const emailValue = email.value.trim();
-    console.log(emailValue);
-    const sujetValue = sujet.value.trim();
-    console.log(sujetValue);
-    const messageValue = message.value.trim();
-    console.log(messageValue);
+    // let formData = new FormData(form);
+    // console.log(formData);
+    //         fetch('php/contact.php', {
+    //             method: "POST",
+    //             body: formData,
 
-    msgError.forEach(error => {
-        error.classList.add('invisible');
-    })
+    // let xhr = new XMLHttpRequest()
+    // xhr.setRequestHeader('Content-type',"application/x-www-form-urlencoded")
+    // xhr.open("POST","./php/contact.php")
+    // xhr.send("nom=tesmoche")
+})
+// })
+// console.log(btn);
 
-    if(nomValue.length < 3 || nomValue.length > 20){
-        console.log('erreur');
-        nom.nextElementSibling.classList.remove('invisible');
-    }else if(sujetValue.length < 3 || sujetValue.length > 50){
-        console.log('erreur');
-        sujet.nextElementSibling.classList.remove('invisible');
-    }else if(messageValue.length < 10 || messageValue.length > 150){
-        console.log('erreur');
-        message.nextElementSibling.classList.remove('invisible');
-    }else {
-        console.log('succes');
-        btn.nextElementSibling.classList.remove('invisible');
-        setTimeout(function(){
-            succes.classList.add('invisible');
-        },3000);
-        form.reset(); 
-    }
-});
+// je detecte la validation du formulaire
+// form.addEventListener('submit', function(e){
+//     e.preventDefault();
+    
+//     // je recupere les valeurs de chacun des inputs
+//     const nomValue = nom.value.trim();
+//     console.log(nomValue);
+//     const emailValue = email.value.trim();
+//     console.log(emailValue);
+//     const sujetValue = sujet.value.trim();
+//     console.log(sujetValue);
+//     const messageValue = message.value.trim();
+//     console.log(messageValue);
+
+//     msgError.forEach(error => {
+//         error.classList.add('invisible');
+//     })
+
+//     if(nomValue.length < 3 || nomValue.length > 20){
+//         console.log('erreur');
+//         nom.nextElementSibling.classList.remove('invisible');
+//     }else if(sujetValue.length < 3 || sujetValue.length > 50){
+//         console.log('erreur');
+//         sujet.nextElementSibling.classList.remove('invisible');
+//     }else if(messageValue.length < 10 || messageValue.length > 150){
+//         console.log('erreur');
+//         message.nextElementSibling.classList.remove('invisible');
+//     }else {
+//         console.log('succes');
+//         btn.nextElementSibling.classList.remove('invisible');
+
+//         let formData = new FormData(form);
+//         fetch('php/contact.php', {
+//             method: "POST",
+//             body: formData,
+//         })
+//         // form.reset();
+        
+        // let data = "nom="+ nom + "&email="+ email + "&sujet="+ sujet;
+        // console.log(data);
+        // let xhr = new XMLHttpRequest();
+
+        // xhr.open("POST", "/contact.php", true);
+        // xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+        // xhr.send(data);
+
+        
+
+        // fetch('../php/contact.php', {
+        //     method: "POST",
+        //     headers: {
+        //         "Content-Type": "application/x-www-form-urlencoded",
+        //     },
+        //     body:{
+            
+        //         "nom" : nomValue,
+        //         "email" : emailValue,
+        //         "sujet" : sujetValue,
+        //         "message" : messageValue,
+        //     },
+        // })
+        //     .then((res) => {
+        //         console.log(res);
+        //     })
+
+
+        // let request = new XMLHttpRequest();
+        // request.open("POST", "./php/contact.php");
+        // request.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+        // request.send(formData);
+
+
+
+        // fetch('./php/contact.php', {
+        //     method: "POST",
+        //     headers: {
+        //         Accept: "application/json",
+        //         "Content-Type": "application/json",
+        //     },
+        //     body: JSON.stringify({
+        //         "nom" : nomValue,
+        //         "email" : emailValue,
+        //         "sujet" : sujetValue,
+        //         "message" : messageValue
+        //     }),
+        // })
+        //     .then((res) => {
+        //         console.log(res);
+        //     })
+//     }
+// });
+
--- php/contact.php
+++ php/contact.php
@@ -1,6 +1,32 @@
 <?php
 
-$retour = mail("ericoriez@live.fr", "Essai", "Coucou les amis !", "");
-if ($retour){
-    echo "<p> email a bien ete envoyer </p>";
-}
+
+$name = $_POST['nom'];
+$email = $_POST['email'];
+$sujet = $_POST['sujet'];
+$message = $_POST['message'];
+
+
+// $mailheader = "From:".$nom."<".$email.">\r\n";
+
+// $mymail = "ericoriez@live.fr";
+
+// mail($myMail, $sujet, $message, $mailheader)
+// or die("Error");
+
+// echo"message send";
+
+
+// $myarray = &$_POST;
+// $nom=$myarray["nom"];
+// var_dump($nom);
+
+
+// if (isset($_POST[$form])){
+//     $retour = mail("ericoriez@live.fr", "Essai", "Coucou les amis !", "");
+//     if ($retour){
+//         echo "<p> email a bien ete envoyer </p>";
+//     }
+// }
+
+?>
